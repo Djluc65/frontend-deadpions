823037d0214b1bee127dcea6c0695d27afae9545
Mackenson Jean Julien <mackensonjeanjulien65@gmail.com>
2026-05-28 09:45:08 +0200

    fix: compileSdk 35

--- src/utils/avatarUtils.js
+++ src/utils/avatarUtils.js
@@ -2,15 +2,15 @@ import { API_URL } from '../config';
 
 export const PREMIUM_AVATARS = [
   { id: 'premium_1', source: require('../../assets/avatars/alba-ballesta-gonzalez-ilustracion.jpg') },
-  { id: 'premium_2', source: require('../../assets/avatars/chris-knight-event-banner-pve-varkolak.jpg') },
+  { id: 'premium_2', source: require('../../assets/images/LogoDeadPions-nobg.png') },
   { id: 'premium_3', source: require('../../assets/avatars/daniel-merticariu-beard-frontexperiment.jpg') },
   { id: 'premium_4', source: require('../../assets/avatars/daniel-merticariu-luna-main-camera-1.jpg') },
-  { id: 'premium_5', source: require('../../assets/avatars/hicham-habchi-sbe3.jpg') },
-  { id: 'premium_6', source: require('../../assets/avatars/ilse-harting-cutie.jpg') },
+  { id: 'premium_5', source: require('../../assets/avatars/lion2.png') },
+  { id: 'premium_6', source: require('../../assets/avatars/lion_drawing_full_body.webp') },
   { id: 'premium_7', source: require('../../assets/avatars/michael-chang-night-king-5hh-2.jpg') },
   { id: 'premium_8', source: require('../../assets/avatars/nikita-cherkezov-delfis-img-5688.jpg') },
   { id: 'premium_9', source: require('../../assets/avatars/nikita-cherkezov-delfis-img-5689.jpg') },
-  { id: 'premium_10', source: require('../../assets/avatars/nikita-cherkezov-delfis-img-5690.jpg') },
+  { id: 'premium_10', source: require('../../assets/images/LogoDeadPions-nobg.png') },
 ];
 
 export const getAvatarSource = (avatarString) => {
